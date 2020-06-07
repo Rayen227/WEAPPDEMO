dc4ec14835155b3e97c2a5a7c950cdc1c56b7a9b
Rayen <16607615570@163.com>
2020-06-07 21:14:25 +0800

    111

--- pages/ranking/ranking.js
+++ pages/ranking/ranking.js
@@ -60,9 +60,9 @@ var cmp = function (obj1, obj2) {
   var val1 = obj1.exp;
   var val2 = obj2.exp;
   if (val1 < val2) {
-    return -1;
-  } else if (val1 > val2) {
     return 1;
+  } else if (val1 > val2) {
+    return -1;
   } else {
     return 0;
   }
